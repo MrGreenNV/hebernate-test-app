242a0df54979e5ce1ca97714bc95fea5cd3a8331
mr Green <mrgreen@gmail.com>
2023-04-25 14:25:13 +0300

    add Person.java update App.java create hibernate.properties.origin

--- src/main/java/ru/averkiev/hibernatetestapp/App.java
+++ src/main/java/ru/averkiev/hibernatetestapp/App.java
@@ -1,5 +1,10 @@
 package ru.averkiev.hibernatetestapp;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.averkiev.hibernatetestapp.model.Person;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Подключение конфигурации hibernate.properties
+        // Указываем класс, помеченный @Entity, тем самым указывая hibernate класс являющийся нашей сущностью
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        // Создаем фабрику сессии из конфигурации
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        // Получаем сессию для работы с hibernate
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            // Начало транзакции
+            session.beginTransaction();
+
+            // Работа с данными
+            Person person = session.get(Person.class, 1);
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            // Завершение транзакции
+            session.getTransaction().commit();
+        } finally {
+            // Закрытие фабрики сессии
+            sessionFactory.close();
+        }
+
     }
 }
